bce2ba8831f139269250cb2610d763aff8565df5
salil0001 <salilsharma.sharma@gmail.com>
2020-08-27 20:00:46 +0530

    wss issue.

--- src/Components/staticLinks.js
+++ src/Components/staticLinks.js
@@ -1,4 +1,4 @@
 export const DEVELOPMENTLINK ="http://localhost:4000/"
 export const DEVELOPMENTLINKWSS ="http://localhost:4000/"
 export const PRODUCTIONLINK ="https://tokyo-server-backend.herokuapp.com/"
-export const PRODUCTIONLINKWEBSOCKET ="ws://tokyo-server-backend.herokuapp.com/"
+export const PRODUCTIONLINKWEBSOCKET ="ws://tokyo-server-backend.herokuapp.com/GetStocksUsersPersonalData"
